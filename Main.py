1c302561142fdf4a0e87d91e96be7d1262f74e0b
BruceKing <49490329+JSYBruce@users.noreply.github.com>
2022-05-28 14:03:39 +0800

    update main

--- Main.py
+++ Main.py
@@ -21,7 +21,7 @@ def runModel(Coinname):
     archmodel = ARCHModel(y = coin_df.returns, distribution = DoubleNormal(), volatility = GARCH())
     result = archmodel.fit(starting_values=[0,0, 0.23763582, 0.2    ,0.2    ,0.9   ,0.5 ,0.4, 5])
     result = ARCHModelResult(result[0], None,  result[1],  result[2], result[3], result[4], result[5], result[6], result[7], result[8], result[9], result[10], result[11], result[12])
-    print(result.summary())
+    return result
     
 def runModelSingle(Coinname, Model):
     with open(Coinname + '_data', 'rb') as f:
